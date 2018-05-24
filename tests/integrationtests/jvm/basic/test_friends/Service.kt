702df094a73e4625be9896e5a9d0f5c5783f2781
Jonathan Gerrish <jonathan@indiekid.org>
2018-05-24 22:40:39 +0100

    issue #69 friend support

---
+++ tests/integrationtests/jvm/basic/test_friends/Service.kt
@@ -0,0 +1,11 @@
+package test
+
+internal const val DEFAULT_FRIEND = "muchacho"
+
+class Service internal constructor(
+  internal val value: String = "hello world"
+) {
+  internal fun iSayHolla(friend: String) {
+    println("holla $friend")
+  }
+}
