a4e902d33504b9a3fe2c46901c67973a74b50f40
hsyed <h.a.syed@gmail.com>
2018-08-17 17:36:47 +0100

    consolate all srcs under src

--- kotlin/builder/integrationtests/KotlinBuilderTestSuite.java
+++
@@ -1,8 +0,0 @@
-package io.bazel.kotlin.builder;
-
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses({KotlinBuilderActionTests.class, KotlinBuilderTests.class})
-public class KotlinBuilderTestSuite {}
