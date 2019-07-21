6b777b46142b1c925541f65f2e34c79db0475bc8
AlexeySoshin <alexeys@wix.com>
2019-07-21 19:15:51 +0100

    Fix friends path + format code

--- src/main/kotlin/io/bazel/kotlin/builder/tasks/jvm/KotlinJvmTaskExecutor.kt
+++ src/main/kotlin/io/bazel/kotlin/builder/tasks/jvm/KotlinJvmTaskExecutor.kt
@@ -21,6 +21,7 @@ import io.bazel.kotlin.builder.utils.*
 import io.bazel.kotlin.builder.utils.jars.JarCreator
 import io.bazel.kotlin.builder.utils.jars.SourceJarCreator
 import io.bazel.kotlin.builder.utils.jars.SourceJarExtractor
+import io.bazel.kotlin.model.CompilationTaskInfo
 import io.bazel.kotlin.model.JvmCompilationTask
 import java.io.File
 import java.nio.file.Files
@@ -30,10 +31,10 @@ import javax.inject.Singleton
 
 @Singleton
 class KotlinJvmTaskExecutor @Inject internal constructor(
-    private val compiler: KotlinToolchain.KotlincInvoker,
-    private val pluginArgsEncoder: KotlinCompilerPluginArgsEncoder,
-    private val javaCompiler: JavaCompiler,
-    private val jDepsGenerator: JDepsGenerator
+        private val compiler: KotlinToolchain.KotlincInvoker,
+        private val pluginArgsEncoder: KotlinCompilerPluginArgsEncoder,
+        private val javaCompiler: JavaCompiler,
+        private val jDepsGenerator: JDepsGenerator
 ) {
     fun execute(context: CompilationTaskContext, task: JvmCompilationTask) {
         val preprocessedTask = task.preProcessingSteps(context)
@@ -45,9 +46,9 @@ class KotlinJvmTaskExecutor @Inject internal constructor(
 
     private fun JvmCompilationTask.preProcessingSteps(context: CompilationTaskContext): JvmCompilationTask {
         ensureDirectories(
-            directories.temp,
-            directories.generatedSources,
-            directories.generatedClasses
+                directories.temp,
+                directories.generatedSources,
+                directories.generatedClasses
         )
         val taskWithAdditionalSources = context.execute("expand sources") { expandWithSourceJarSources() }
         return context.execute({
@@ -59,28 +60,28 @@ class KotlinJvmTaskExecutor @Inject internal constructor(
         Paths.get(outputs.srcjar).also { sourceJarPath ->
             Files.createFile(sourceJarPath)
             SourceJarCreator(
-                sourceJarPath
+                    sourceJarPath
             ).also { creator ->
                 // This check asserts that source jars were unpacked if present.
                 check(
-                    inputs.sourceJarsList.isEmpty() ||
-                            Files.exists(Paths.get(directories.temp).resolve("_srcjars"))
+                        inputs.sourceJarsList.isEmpty() ||
+                                Files.exists(Paths.get(directories.temp).resolve("_srcjars"))
                 )
                 listOf(
-                    // Any (input) source jars should already have been expanded so do not add them here.
-                    inputs.javaSourcesList.stream(),
-                    inputs.kotlinSourcesList.stream()
+                        // Any (input) source jars should already have been expanded so do not add them here.
+                        inputs.javaSourcesList.stream(),
+                        inputs.kotlinSourcesList.stream()
                 ).stream()
-                    .flatMap { it.map { p -> Paths.get(p) } }
-                    .also { creator.addSources(it) }
+                        .flatMap { it.map { p -> Paths.get(p) } }
+                        .also { creator.addSources(it) }
                 creator.execute()
             }
         }
     }
 
     private fun JvmCompilationTask.runAnnotationProcessor(
-        context: CompilationTaskContext,
-        printOnSuccess: Boolean = true
+            context: CompilationTaskContext,
+            printOnSuccess: Boolean = true
     ): List<String> {
         check(inputs.processorsList.isNotEmpty()) { "method called without annotation processors" }
         return getCommonArgs().let { args ->
@@ -97,55 +98,55 @@ class KotlinJvmTaskExecutor @Inject internal constructor(
     private fun JvmCompilationTask.getCommonArgs(): MutableList<String> {
         val args = mutableListOf<String>()
         args.addAll(
-            "-cp", inputs.joinedClasspath,
-            "-api-version", info.toolchainInfo.common.apiVersion,
-            "-language-version", info.toolchainInfo.common.languageVersion,
-            "-jvm-target", info.toolchainInfo.jvm.jvmTarget,
-            "-Xfriend-paths=${info.friendPathsList.joinToString(File.pathSeparator)}"
+                "-cp", inputs.joinedClasspath,
+                "-api-version", info.toolchainInfo.common.apiVersion,
+                "-language-version", info.toolchainInfo.common.languageVersion,
+                "-jvm-target", info.toolchainInfo.jvm.jvmTarget,
+                "-Xfriend-paths=${info.friends()}"
         )
 
         args
-            .addAll("-module-name", info.moduleName)
-            .addAll("-d", directories.classes)
+                .addAll("-module-name", info.moduleName)
+                .addAll("-d", directories.classes)
 
         info.passthroughFlags?.takeIf { it.isNotBlank() }?.also { args.addAll(it.split(" ")) }
         return args
     }
 
     private fun JvmCompilationTask.runAnnotationProcessors(
-        context: CompilationTaskContext
+            context: CompilationTaskContext
     ): JvmCompilationTask =
-        if (inputs.processorsList.isEmpty()) {
-            this
-        } else {
-            runAnnotationProcessor(context, printOnSuccess = !context.isTracing).let { outputLines ->
-                // if tracing is enabled the output should be formatted in a special way, if we aren't tracing then any
-                // compiler output would make it's way to the console as is.
-                if (context.isTracing) {
-                    context.printLines("kapt output", outputLines)
+            if (inputs.processorsList.isEmpty()) {
+                this
+            } else {
+                runAnnotationProcessor(context, printOnSuccess = !context.isTracing).let { outputLines ->
+                    // if tracing is enabled the output should be formatted in a special way, if we aren't tracing then any
+                    // compiler output would make it's way to the console as is.
+                    if (context.isTracing) {
+                        context.printLines("kapt output", outputLines)
+                    }
+                    expandWithGeneratedSources()
                 }
-                expandWithGeneratedSources()
             }
-        }
 
     /**
      * Produce the primary output jar.
      */
     private fun JvmCompilationTask.createOutputJar() =
-        JarCreator(
-            path = Paths.get(outputs.jar),
-            normalize = true,
-            verbose = false
-        ).also {
-            it.addDirectory(Paths.get(directories.classes))
-            it.addDirectory(Paths.get(directories.generatedClasses))
-            it.setJarOwner(info.label, info.bazelRuleKind)
-            it.execute()
-        }
+            JarCreator(
+                    path = Paths.get(outputs.jar),
+                    normalize = true,
+                    verbose = false
+            ).also {
+                it.addDirectory(Paths.get(directories.classes))
+                it.addDirectory(Paths.get(directories.generatedClasses))
+                it.setJarOwner(info.label, info.bazelRuleKind)
+                it.execute()
+            }
 
     private fun JvmCompilationTask.compileAll(context: CompilationTaskContext) {
         ensureDirectories(
-            directories.classes
+                directories.classes
         )
         var kotlinError: CompilationStatusException? = null
         var result: List<String>? = null
@@ -169,55 +170,59 @@ class KotlinJvmTaskExecutor @Inject internal constructor(
      * Compiles Kotlin sources to classes. Does not compile Java sources.
      */
     private fun JvmCompilationTask.compileKotlin(context: CompilationTaskContext, printOnFail: Boolean = true) =
-        getCommonArgs().let { args ->
-            args.addAll(inputs.javaSourcesList)
-            args.addAll(inputs.kotlinSourcesList)
-            context.executeCompilerTask(args, compiler::compile, printOnFail = printOnFail)
-        }
+            getCommonArgs().let { args ->
+                args.addAll(inputs.javaSourcesList)
+                args.addAll(inputs.kotlinSourcesList)
+                context.executeCompilerTask(args, compiler::compile, printOnFail = printOnFail)
+            }
 
     /**
      * If any srcjars were provided expand the jars sources and create a new [JvmCompilationTask] with the
      * Java and Kotlin sources merged in.
      */
     private fun JvmCompilationTask.expandWithSourceJarSources(): JvmCompilationTask =
-        if (inputs.sourceJarsList.isEmpty())
-            this
-        else expandWithSources(
-            SourceJarExtractor(
-                destDir = Paths.get(directories.temp).resolve("_srcjars"),
-                fileMatcher = IS_JVM_SOURCE_FILE
-            ).also {
-                it.jarFiles.addAll(inputs.sourceJarsList.map { p -> Paths.get(p) })
-                it.execute()
-            }.sourcesList.iterator()
-        )
+            if (inputs.sourceJarsList.isEmpty())
+                this
+            else expandWithSources(
+                    SourceJarExtractor(
+                            destDir = Paths.get(directories.temp).resolve("_srcjars"),
+                            fileMatcher = IS_JVM_SOURCE_FILE
+                    ).also {
+                        it.jarFiles.addAll(inputs.sourceJarsList.map { p -> Paths.get(p) })
+                        it.execute()
+                    }.sourcesList.iterator()
+            )
 
     /**
      * Create a new [JvmCompilationTask] with sources found in the generatedSources directory. This should be run after
      * annotation processors have been run.
      */
     private fun JvmCompilationTask.expandWithGeneratedSources(): JvmCompilationTask =
-        expandWithSources(
-            File(directories.generatedSources).walkTopDown()
-                .filter { it.isFile }
-                .map { it.path }
-                .iterator()
-        )
+            expandWithSources(
+                    File(directories.generatedSources).walkTopDown()
+                            .filter { it.isFile }
+                            .map { it.path }
+                            .iterator()
+            )
 
     private fun JvmCompilationTask.expandWithSources(sources: Iterator<String>): JvmCompilationTask =
-        updateBuilder { builder ->
-            sources.partitionJvmSources(
-                { builder.inputsBuilder.addKotlinSources(it) },
-                { builder.inputsBuilder.addJavaSources(it) })
-        }
+            updateBuilder { builder ->
+                sources.partitionJvmSources(
+                        { builder.inputsBuilder.addKotlinSources(it) },
+                        { builder.inputsBuilder.addJavaSources(it) })
+            }
 
     private fun JvmCompilationTask.updateBuilder(
-        block: (JvmCompilationTask.Builder) -> Unit
+            block: (JvmCompilationTask.Builder) -> Unit
     ): JvmCompilationTask =
-        toBuilder().let {
-            block(it)
-            it.build()
-        }
+            toBuilder().let {
+                block(it)
+                it.build()
+            }
 }
+
+private fun CompilationTaskInfo.friends() = friendPathsList
+        .map { Paths.get(it).toAbsolutePath() }
+        .joinToString(File.pathSeparator)
 
 
