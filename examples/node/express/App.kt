734f52a92051de15389111d1281dc4d62fa5ace3
Jason Feinstein <jwf@google.com>
2019-10-26 22:13:48 -0700

    Add a more nuanced approach to pulling kt_js_import files out of the jar.

--- examples/node/express/App.kt
+++ examples/node/express/App.kt
@@ -1,13 +1,24 @@
 package express
 
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.*
+
 @JsModule("express")
 external fun express(): dynamic
 
 val app = express()
 
+@ExperimentalCoroutinesApi
 fun main(args: Array<String>) {
+    val scope = CoroutineScope(Dispatchers.Default)
+
     // register the routes.
-    routes(app)
+    val hitCountChannel = routes(app)
+    scope.launch {
+        hitCountChannel.consumeEach { 
+            println("Hits so far: $it")
+        }
+    }
 
     app.listen(3000, {
         println("Listening on port 3000")
