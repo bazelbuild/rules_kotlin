af30345d423b92a794ec9c39bdfb4ba6836e0cfa
Corbin Smith <corbin@squareup.com>
2020-03-02 11:36:46 -0500

    Introduce CompilationArgs to improve compilation argument readability

--- src/main/kotlin/io/bazel/kotlin/builder/tasks/jvm/compilation_task.kt
+++ src/main/kotlin/io/bazel/kotlin/builder/tasks/jvm/compilation_task.kt
@@ -17,7 +17,6 @@
 // Provides extensions for the JvmCompilationTask protocol buffer.
 package io.bazel.kotlin.builder.tasks.jvm
 
-import io.bazel.kotlin.builder.toolchain.CompilationStatusException
 import io.bazel.kotlin.builder.toolchain.CompilationTaskContext
 import io.bazel.kotlin.builder.toolchain.KotlinCompilerPluginArgsEncoder
 import io.bazel.kotlin.builder.toolchain.KotlinToolchain
@@ -27,38 +26,31 @@ import io.bazel.kotlin.builder.utils.ensureDirectories
 import io.bazel.kotlin.builder.utils.jars.JarCreator
 import io.bazel.kotlin.builder.utils.jars.SourceJarCreator
 import io.bazel.kotlin.builder.utils.jars.SourceJarExtractor
-import io.bazel.kotlin.builder.utils.joinedClasspath
 import io.bazel.kotlin.builder.utils.partitionJvmSources
 import io.bazel.kotlin.model.JvmCompilationTask
 import java.io.File
 import java.nio.file.Files
+import java.nio.file.Path
 import java.nio.file.Paths
 
 /**
  * Return a list with the common arguments.
  */
-internal fun JvmCompilationTask.getCommonArgs(): MutableList<String> {
-  val friendPaths = info.friendPathsList.map { Paths.get(it).toAbsolutePath() }
-  val args = mutableListOf<String>()
-  args.addAll(baseArgs() + listOf(
-      "-Xfriend-paths=${friendPaths.joinToString(X_FRIENDS_PATH_SEPARATOR)}",
-      "-d", directories.classes))
-  info.passthroughFlags?.takeIf { it.isNotBlank() }?.let { args.addAll(it.split(" ")) }
-  return args
-}
+internal fun JvmCompilationTask.commonArgs(): CompilationArgs = baseArgs()
+    .absolutePaths(info.friendPathsList) {
+      "-Xfriend-paths=${it.joinToString(X_FRIENDS_PATH_SEPARATOR)}"
+    }
+    .flag("-d", directories.classes)
+    .givenNotEmpty(info.passthroughFlags) { it.split(" ") }
 
-internal fun JvmCompilationTask.baseArgs(): List<String> {
-  return listOf(
-      "-cp", inputs.joinedClasspath
-      .split(File.pathSeparator)
-      .map { Paths.get(it).toAbsolutePath() }
-      .joinToString(File.pathSeparator),
-      "-api-version", info.toolchainInfo.common.apiVersion,
-      "-language-version", info.toolchainInfo.common.languageVersion,
-      "-jvm-target", info.toolchainInfo.jvm.jvmTarget,
-      "-module-name", info.moduleName
-  )
-}
+internal fun JvmCompilationTask.baseArgs(): CompilationArgs = CompilationArgs()
+    .flag("-cp").absolutePaths(inputs.classpathList) {
+      it.map(Path::toString).joinToString(File.pathSeparator)
+    }
+    .flag("-api-version", info.toolchainInfo.common.apiVersion)
+    .flag("-language-version", info.toolchainInfo.common.languageVersion)
+    .flag("-jvm-target", info.toolchainInfo.jvm.jvmTarget)
+    .flag("-module-name", info.moduleName)
 
 internal fun JvmCompilationTask.preProcessingSteps(context: CompilationTaskContext): JvmCompilationTask {
   ensureDirectories(
@@ -103,23 +95,22 @@ internal fun JvmCompilationTask.runAnnotationProcessors(
   } else {
     return context.execute("kapt (${inputs.processorsList.joinToString(", ")})")
     {
-      getCommonArgs().let { args ->
-        args.addAll(pluginArgsEncoder.encode(context,
-            this))
-        args.addAll(inputs.kotlinSourcesList)
-        args.addAll(inputs.javaSourcesList)
-        context.executeCompilerTask(
-            args,
-            compiler::compile,
-            printOnSuccess = context.whenTracing { false } ?: true)
-      }.let { outputLines ->
-        // if tracing is enabled the output should be formatted in a special way, if we aren't tracing then any
-        // compiler output would make it's way to the console as is.
-        context.whenTracing {
-          printLines("kapt output", outputLines)
-        }
-        expandWithGeneratedSources()
-      }
+      commonArgs()
+          .values(pluginArgsEncoder.encode(context, this))
+          .values(inputs.kotlinSourcesList)
+          .values(inputs.javaSourcesList).list().let { args ->
+            context.executeCompilerTask(
+                args,
+                compiler::compile,
+                printOnSuccess = context.whenTracing { false } ?: true)
+          }.let { outputLines ->
+            // if tracing is enabled the output should be formatted in a special way, if we aren't
+            // tracing then any compiler output would make it's way to the console as is.
+            context.whenTracing {
+              printLines("kapt output", outputLines)
+            }
+            return@let expandWithGeneratedSources()
+          }
     }
   }
 }
@@ -147,11 +138,12 @@ internal fun JvmCompilationTask.compileKotlin(
   compiler: KotlinToolchain.KotlincInvoker,
   printOnFail: Boolean = true
 ) =
-    getCommonArgs().let { args ->
-      args.addAll(inputs.javaSourcesList)
-      args.addAll(inputs.kotlinSourcesList)
-      context.executeCompilerTask(args, compiler::compile, printOnFail = printOnFail)
-    }
+    commonArgs()
+        .values(inputs.javaSourcesList)
+        .values(inputs.kotlinSourcesList)
+        .list().let { args ->
+          return@let context.executeCompilerTask(args, compiler::compile, printOnFail = printOnFail)
+        }
 
 /**
  * If any srcjars were provided expand the jars sources and create a new [JvmCompilationTask] with the
