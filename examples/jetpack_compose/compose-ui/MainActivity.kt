8fc6266cebc5699793c3426d32f8b86283bd518e
Benjamin Lee <ben@ben.cm>
2021-01-13 21:46:57 -0800

    Fix the greeting so that it doesn't say 'Hello' twice

--- examples/jetpack_compose/compose-ui/MainActivity.kt
+++ examples/jetpack_compose/compose-ui/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting("Hello world")
+            Greeting("world")
         }
     }
 }
