19c850a062b630054ac5756761be633961427c8d
Marc Plano-Lesay <mplano@google.com>
2021-04-20 15:08:50 +1000

    Update Jetpack Compose to 1.0.0-beta04

--- examples/jetpack_compose/compose-ui/MainActivity.kt
+++ examples/jetpack_compose/compose-ui/MainActivity.kt
@@ -1,10 +1,10 @@
 package cm.ben.android.bazel.compose.example.ui
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.setContent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
