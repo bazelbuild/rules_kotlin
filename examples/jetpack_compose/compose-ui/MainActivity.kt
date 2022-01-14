b5eac740e94a929afa7869df52f4ae41568eaeb3
Benjamin Lee <ben@ben.cm>
2022-01-14 13:29:36 -0800

    This is fine

--- examples/jetpack_compose/compose-ui/MainActivity.kt
+++ examples/jetpack_compose/compose-ui/MainActivity.kt
@@ -5,15 +5,17 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            Greeting("world")
-        }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      Greeting("world")
     }
+  }
 }
 
+@Preview
 @Composable
 fun Greeting(name: String) = Text(text = "Hello $name!")
