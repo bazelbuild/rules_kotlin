503434f0a03a9cc578b926d880484ff804799d3b
Christian Edward Gruber <gruber@squareup.com>
2022-01-24 08:34:39 -0800

    Make the underlying _kt library public. (#668)
    * Make the underlying _kt library public.
    
    Android kotlin libraries are a macro wrapper, and the underlying kotlin library (for target //foo:bar -> //foo:bar_kt) is marked private. Since the associates= (friendly) internal feature of kotlin requires access to a kotlin library, one is forced to often link test targets to the underlying android _kt backing target, which breaks if you're not in hte same package.
    
    This PR makes it public (which sucks, but kind of is necessary until we can eliminate teh macro-based 'android-sandwich' thing we presently do, so that the associates= feature can work at all in kt_android_* targest.
    
    Also adds an example (which fails without the change) to the android example.
    
    Fixes #540
    
    * buildifier.

--- examples/android/libKtAndroid/src/main/java/examples/android/lib/TestKtValue.kt
+++ examples/android/libKtAndroid/src/main/java/examples/android/lib/TestKtValue.kt
@@ -5,11 +5,17 @@ import com.google.auto.value.AutoValue
 @AutoValue
 abstract class TestKtValue {
   abstract fun name(): String
-  fun builder(): Builder = AutoValue_TestKtValue.Builder()
+  internal fun nameInternal() = "${name()}_internal"
 
   @AutoValue.Builder
   abstract class Builder {
     abstract fun setName(name: String): Builder
     abstract fun build(): TestKtValue
   }
+  companion object {
+    fun create(builderFunction: Builder.() -> Unit) = AutoValue_TestKtValue.Builder().also {
+        builderFunction.invoke(it)
+      }.build()
+  }
+
 }
