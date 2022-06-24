c5c085bec09ccf2fd668fa56195b1b895ca5ddc7
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2022-06-24 22:57:03 +0200

    Apply formatting as per linter
    This makes the tests in this
    example pass for both windows
    and linux

--- examples/trivial/app/app.kt
+++ examples/trivial/app/app.kt
@@ -8,21 +8,18 @@ import graphql.GraphQL
 data class Foo(val name: String, val age: Int)
 
 class Query {
-  fun foo(bar: String?) = Foo("$bar!", 42)
+    fun foo(bar: String?) = Foo("$bar!", 42)
 }
 
 class MyApp {
-  companion object {
-    @JvmStatic
-    fun main(args: Array<String>) {
-      val schema = toSchema(queries = listOf(TopLevelObject(Query())),
-          config = SchemaGeneratorConfig(listOf("app")))
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val schema = toSchema(queries = listOf(TopLevelObject(Query())), config = SchemaGeneratorConfig(listOf("app")))
+            val graphql = GraphQL.newGraphQL(schema).build()
+            val result = graphql.execute("""{ foo(bar: "baz") { name, age } }""").toSpecification()
 
-      val graphql = GraphQL.newGraphQL(schema).build()
-
-      val result = graphql.execute("""{ foo(bar: "baz") { name, age } }""").toSpecification()
-
-      println(result)
+            println(result)
+        }
     }
-  }
 }
