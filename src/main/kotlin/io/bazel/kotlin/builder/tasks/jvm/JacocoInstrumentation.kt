d6a287a4cfb611d5ac4313f9bf3ef43ac519095c
Benjamin Lee <ben@ben.cm>
2022-09-16 14:53:25 -0700

    Fix

--- src/main/kotlin/io/bazel/kotlin/builder/tasks/jvm/JacocoInstrumentation.kt
+++ src/main/kotlin/io/bazel/kotlin/builder/tasks/jvm/JacocoInstrumentation.kt
@@ -12,63 +12,66 @@ import java.nio.file.Paths
 import java.nio.file.SimpleFileVisitor
 import java.nio.file.attribute.BasicFileAttributes
 
-internal fun JvmCompilationTask.createCoverageInstrumentedJar() {
-  val instrumentedClassesDirectory = Paths.get(directories.coverageMetadataClasses)
-  Files.createDirectories(instrumentedClassesDirectory)
+object JacocoInstrumentation {
 
-  val instr = Instrumenter(OfflineInstrumentationAccessGenerator())
+  internal fun JvmCompilationTask.createCoverageInstrumentedJar() {
+    val instrumentedClassesDirectory = Paths.get(directories.coverageMetadataClasses)
+    Files.createDirectories(instrumentedClassesDirectory)
 
-  instrumentRecursively(instr, instrumentedClassesDirectory, Paths.get(directories.classes))
-  instrumentRecursively(instr, instrumentedClassesDirectory, Paths.get(directories.javaClasses))
-  instrumentRecursively(
-    instr,
-    instrumentedClassesDirectory,
-    Paths.get(directories.generatedClasses),
-  )
+    val instr = Instrumenter(OfflineInstrumentationAccessGenerator())
 
-  val pathsForCoverage = instrumentedClassesDirectory.resolve(
-    "${Paths.get(outputs.jar).fileName}-paths-for-coverage.txt",
-  )
-  Files.write(
-    pathsForCoverage,
-    inputs.javaSourcesList + inputs.kotlinSourcesList,
-  )
+    instrumentRecursively(instr, instrumentedClassesDirectory, Paths.get(directories.classes))
+    instrumentRecursively(instr, instrumentedClassesDirectory, Paths.get(directories.javaClasses))
+    instrumentRecursively(
+      instr,
+      instrumentedClassesDirectory,
+      Paths.get(directories.generatedClasses),
+    )
 
-  JarCreator(
-    path = Paths.get(outputs.jar),
-    normalize = true,
-    verbose = false,
-  ).also {
-    it.addDirectory(Paths.get(directories.classes))
-    it.addDirectory(Paths.get(directories.javaClasses))
-    it.addDirectory(Paths.get(directories.generatedClasses))
-    it.addDirectory(instrumentedClassesDirectory)
-    it.setJarOwner(info.label, info.bazelRuleKind)
-    it.execute()
-  }
-}
+    val pathsForCoverage = instrumentedClassesDirectory.resolve(
+      "${Paths.get(outputs.jar).fileName}-paths-for-coverage.txt",
+    )
+    Files.write(
+      pathsForCoverage,
+      inputs.javaSourcesList + inputs.kotlinSourcesList,
+    )
 
-private fun instrumentRecursively(instr: Instrumenter, metadataDir: Path, root: Path) {
-  val visitor = object : SimpleFileVisitor<Path>() {
-    override fun visitFile(file: Path, attrs: BasicFileAttributes): FileVisitResult {
-      if (file.toFile().extension != "class") {
-        return FileVisitResult.CONTINUE
-      }
-
-      val absoluteUninstrumentedCopy = Paths.get("$file.uninstrumented")
-      val uninstrumentedCopy = metadataDir.resolve(root.relativize(absoluteUninstrumentedCopy))
-
-      Files.createDirectories(uninstrumentedCopy.parent)
-      Files.move(file, uninstrumentedCopy)
-
-      Files.newInputStream(uninstrumentedCopy).buffered().use { input ->
-        Files.newOutputStream(file).buffered().use { output ->
-          instr.instrument(input, output, file.toString())
-        }
-      }
-
-      return FileVisitResult.CONTINUE
+    JarCreator(
+      path = Paths.get(outputs.jar),
+      normalize = true,
+      verbose = false,
+    ).also {
+      it.addDirectory(Paths.get(directories.classes))
+      it.addDirectory(Paths.get(directories.javaClasses))
+      it.addDirectory(Paths.get(directories.generatedClasses))
+      it.addDirectory(instrumentedClassesDirectory)
+      it.setJarOwner(info.label, info.bazelRuleKind)
+      it.execute()
     }
   }
-  Files.walkFileTree(root, visitor)
+
+  private fun instrumentRecursively(instr: Instrumenter, metadataDir: Path, root: Path) {
+    val visitor = object : SimpleFileVisitor<Path>() {
+      override fun visitFile(file: Path, attrs: BasicFileAttributes): FileVisitResult {
+        if (file.toFile().extension != "class") {
+          return FileVisitResult.CONTINUE
+        }
+
+        val absoluteUninstrumentedCopy = Paths.get("$file.uninstrumented")
+        val uninstrumentedCopy = metadataDir.resolve(root.relativize(absoluteUninstrumentedCopy))
+
+        Files.createDirectories(uninstrumentedCopy.parent)
+        Files.move(file, uninstrumentedCopy)
+
+        Files.newInputStream(uninstrumentedCopy).buffered().use { input ->
+          Files.newOutputStream(file).buffered().use { output ->
+            instr.instrument(input, output, file.toString())
+          }
+        }
+
+        return FileVisitResult.CONTINUE
+      }
+    }
+    Files.walkFileTree(root, visitor)
+  }
 }
