67645b33a41dffc298c42ccde6f616fd0e1c4941
Nick Korostelev <nickkor@gmail.com>
2022-10-05 13:05:16 -0700

    initial commit of deps sample app

---
+++ examples/deps/libKt1/src/main/kt/examples/deps/KtDummy1.kt
@@ -0,0 +1,7 @@
+package examples.deps.libKt1
+
+class KtDummy1 {
+  fun dummy() {
+    System.out.println("dummy")
+  }
+}
---
+++ examples/deps/libKt3/src/main/kt/examples/deps/KtDummy3.kt
@@ -0,0 +1,7 @@
+package examples.deps.libKt3
+
+class KtDummy3 {
+  fun dummy() {
+    System.out.println("dummy")
+  }
+}
