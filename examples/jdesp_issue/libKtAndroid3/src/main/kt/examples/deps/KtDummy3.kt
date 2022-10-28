35fd58f59c44428f1c043aa8832779dbe22369d0
Nick Korostelev <nickkor@gmail.com>
2022-10-27 17:53:24 -0700

    Add jdeps issue example

---
+++ examples/jdesp_issue/libKtAndroid3/src/main/kt/examples/deps/KtDummy3.kt
@@ -0,0 +1,13 @@
+package examples.deps.libktandroid3
+
+open class KtDummy3 {
+
+  companion object {
+
+    private val resourceId = R.string.dummy3;
+  }
+
+  open fun dummy() {
+    System.out.println("dummy")
+  }
+}
