0b51189250c48902727f71fdfed39bf10371979b
Corbin McNeely-Smith <58151731+restingbull@users.noreply.github.com>
2023-05-14 19:03:17 -0500

    Generate capability file from compiler jar.
    To generate a new set of flags, run:
    `bazel run //src/main/kotlin/io/bazel/kotlin/generate:kotlin_release_options`

--- src/main/kotlin/io/bazel/kotlin/generate/WriteKotlincCapabilities.kt
+++ src/main/kotlin/io/bazel/kotlin/generate/WriteKotlincCapabilities.kt
@@ -5,39 +5,71 @@ import org.jetbrains.kotlin.cli.common.arguments.Argument
 import org.jetbrains.kotlin.cli.common.arguments.K2JVMCompilerArguments
 import org.jetbrains.kotlin.config.LanguageVersion
 import java.nio.file.FileSystems
-import kotlin.io.path.readText
+import java.nio.file.Files
+import java.time.Year
+import kotlin.io.path.exists
 import kotlin.io.path.writeText
 
+/**
+ * Generates a list of kotlinc flags from the K2JVMCompilerArguments on the classpath.
+ */
 object WriteKotlincCapabilities {
 
   @JvmStatic
   fun main(vararg args: String) {
+    // TODO: Replace with a real option parser
+    val options = args.asSequence()
+      .flatMap { t -> t.split("=", limit = 1) }
+      .chunked(2)
+      .fold(mutableMapOf<String, MutableList<String>>()) { m, (key, value) ->
+        m.apply {
+          computeIfAbsent(key) { mutableListOf() }.add(value)
+        }
+      }
+
     val instance = K2JVMCompilerArguments()
 
     val capabilitiesName = LanguageVersion.LATEST_STABLE.run {
-      "capabilities_${major}.${minor}.bzl"
+      "capabilities_${major}.${minor}.bzl.com_github_jetbrains_kotlin.bazel"
     }
 
-    FileSystems.getDefault().getPath("/tmp/$capabilitiesName").apply {
-      writeText(
-        K2JVMCompilerArguments::class.members.asSequence()
-          .map { member ->
-            member.annotations.find { it is Argument }?.let { argument ->
-              member to (argument as Argument)
+
+    val env_pattern = Regex("\\$\\{(\\w+)}")
+    val capabilitiesDirectory = options["--out"]
+      ?.first()
+      ?.let { env ->
+        env_pattern.replace(env) {
+          System.getenv(it.groups[1]?.value)
+        }
+      }
+      ?: error("--out is required")
+
+    FileSystems.getDefault()
+      .getPath("$capabilitiesDirectory/$capabilitiesName")
+      .apply {
+        if (!parent.exists()) {
+          Files.createDirectories(parent)
+        }
+        writeText(
+          K2JVMCompilerArguments::class.members.asSequence()
+            .map { member ->
+              member.annotations.find { it is Argument }?.let { argument ->
+                member to (argument as Argument)
+              }
             }
-          }
-          .filterNotNull()
-          .map { (member, argument) ->
-            KotlincCapability(
-              flag = argument.value,
-              default = "${member.call(instance)}",
-              type = member.returnType.toString(),
-            )
-          }.asCapabilities().asCapabilitiesBzl())
-    }.let{
-      println(it)
-      println(it.readText())
-    }
+            .filterNotNull()
+            .map { (member, argument) ->
+              KotlincCapability(
+                flag = argument.value,
+                default = "${member.call(instance)}",
+                type = member.returnType.toString(),
+              )
+            }.asCapabilities().asCapabilitiesBzl(),
+        )
+      }
+      .let {
+        println("Wrote to $it")
+      }
   }
 
   private class KotlincCapabilities(capabilities: Iterable<KotlincCapability>) :
@@ -47,14 +79,36 @@ object WriteKotlincCapabilities {
       fun Sequence<KotlincCapability>.asCapabilities() = KotlincCapabilities(toList())
     }
 
-    fun asCapabilitiesBzl() = joinToString(",", prefix = "[", postfix = "]") { "\"${it.flag}\"" }
+    fun asCapabilitiesBzl() = HEADER + "\n" + joinToString(
+      ",\n    ",
+      prefix = "KOTLIN_OPTS = [\n    ",
+      postfix = "\n]",
+      transform = KotlincCapability::asCapabilityFlag,
+    )
   }
 
   private data class KotlincCapability(
-    val flag: String,
-    val default: String,
-    val type: String,
+    private val flag: String,
+    private val default: String,
+    private val type: String,
   ) {
-    fun asCapabilityFlag() = flag
+    fun asCapabilityFlag() = "\"${flag}\""
   }
+
+
+  private val HEADER = """
+    # Copyright ${Year.now()} The Bazel Authors. All rights reserved.
+    #
+    # Licensed under the Apache License, Version 2.0 (the "License");
+    # you may not use this file except in compliance with the License.
+    # You may obtain a copy of the License at
+    #
+    #    http://www.apache.org/licenses/LICENSE-2.0
+    #
+    # Unless required by applicable law or agreed to in writing, software
+    # distributed under the License is distributed on an "AS IS" BASIS,
+    # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    # See the License for the specific language governing permissions and
+    # limitations under the License.
+  """.trimIndent()
 }
