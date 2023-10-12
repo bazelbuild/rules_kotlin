03cd86325516d4f2823c528bcababaaabee3b3a8
Benjamin Lee <ben@ben.cm>
2023-10-12 12:11:25 -0700

    More BCR configuration changes

---
+++ .github/workflows/release_prep.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -o errexit -o nounset -o pipefail
+
+# Set by GH actions, see
+# https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
+TAG=${GITHUB_REF_NAME}
+# The prefix is chosen to match what GitHub generates for source archives
+PREFIX="rules_kotlin-${TAG:1}"
+ARCHIVE="rules_kotlin-$TAG.tar.gz"
+bazelisk build //:rules_kotlin_release
+cp bazel-bin/rules_kotlin_release.tgz > $ARCHIVE
+SHA=$(shasum -a 256 $ARCHIVE | awk '{print $1}')
+
+cat << EOF
+## Using Bzlmod with Bazel 6
+
+1. Enable with \`common --enable_bzlmod\` in \`.bazelrc\`.
+2. Add to your \`MODULE.bazel\` file:
+
+\`\`\`starlark
+bazel_dep(name = "rules_kotlin", version = "${TAG:1}")
+\`\`\`
+
+## Using WORKSPACE
+
+Paste this snippet into your `WORKSPACE.bazel` file:
+
+\`\`\`starlark
+load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
+http_archive(
+    name = "rules_kotlin",
+    sha256 = "${SHA}",
+    strip_prefix = "${PREFIX}",
+    url = "https://github.com/buildfoundation/rules_kotlin/releases/download/${TAG}/${ARCHIVE}",
+)
+EOF
+
+echo "\`\`\`"
