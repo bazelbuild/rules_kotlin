49d72a02b28202e0ee666684fe370196d27939d3
Chris Heisterkamp <cheister@squareup.com>
2023-11-30 11:26:36 -0800

    Pass javac options to Kapt javacArguments so the javac_options warn='off' option works for annotation processors that print warnings

--- src/main/kotlin/io/bazel/kotlin/builder/tasks/KotlinBuilder.kt
+++ src/main/kotlin/io/bazel/kotlin/builder/tasks/KotlinBuilder.kt
@@ -53,6 +53,7 @@ class KotlinBuilder
       ) : Flag {
         TARGET_LABEL("--target_label"),
         CLASSPATH("--classpath"),
+        JAVAC_OPTS("--javacopts"),
         DIRECT_DEPENDENCIES("--direct_dependencies"),
         DEPS_ARTIFACTS("--deps_artifacts"),
         SOURCES("--sources"),
@@ -288,6 +289,8 @@ class KotlinBuilder
             ?.also {
               addAllSourceJars(it)
             }
+
+          addAllJavacFlags(argMap.optional(KotlinBuilderFlags.JAVAC_OPTS) ?: emptyList())
         }
 
         with(root.infoBuilder) {
