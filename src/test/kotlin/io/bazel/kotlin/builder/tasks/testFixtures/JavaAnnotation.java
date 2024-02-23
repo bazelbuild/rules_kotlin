d4a9956186b5d2a56796ab14a65a39197aded0e9
Ben Lee <ben@ben.cm>
2024-02-22 16:08:07 -0800

    Expand java annotation test cases

--- src/test/kotlin/io/bazel/kotlin/builder/tasks/testFixtures/JavaAnnotation.java
+++ src/test/kotlin/io/bazel/kotlin/builder/tasks/testFixtures/JavaAnnotation.java
@@ -2,7 +2,10 @@ package something;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.reflect.Array;
 
 @Retention(RetentionPolicy.RUNTIME)
 public @interface JavaAnnotation {
+
+    Class<?>[] modules() default {};
 }
