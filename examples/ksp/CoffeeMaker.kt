4b96d2ad9abb47672778b09abfd49b0a68f0007e
Sebastian Zalewski <zalewski.se@gmail.com>
2024-03-19 00:01:47 +0100

    Add dagger to ksp sample project

---
+++ examples/ksp/CoffeeMaker.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2018 The Bazel Authors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package coffee
+
+import dagger.Lazy
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class CoffeeMaker @Inject internal constructor(
+  // Create a possibly costly heater only when we use it.
+  private val heater: Lazy<Heater>,
+  private val pump: Pump,
+  private val string: String
+) {
+
+  suspend fun brew() {
+    // this function is async to verify intellij support for coroutines.
+    withContext(Dispatchers.Default) {
+      heater.get().on()
+      pump.pump()
+      println(" [_]P coffee! [_]P ")
+      println(string)
+      heater.get().off()
+    }
+  }
+}
